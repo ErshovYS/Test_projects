7a105b470b80b80ec2954de0a1d81e4b6f28ac6f
ErshovYS <yurikzz@narod.ru>
2018-05-09 13:35:15 +0300

    Task 4. Recursive function

--- Agima_test/agima-test.py
+++ Agima_test/agima-test.py
@@ -2,12 +2,22 @@
 import json
 import sys
 
-with open('agima/source.json','r') as f:
-    json_obj = json.loads(f.read())
+#Если внутри списка найден список, то функция вызывается рекурсивно с найденным списком
+def rec_func(json_obj):
     sys.stdout.write('<ul>\n')
     for item in json_obj:
         sys.stdout.write('<li>\n')
         for k in item.keys():
-            sys.stdout.write('<' + k + '>' + item[k] + '</' + k + '>\n')
+            if type(item[k]) == list:
+                sys.stdout.write('<' + k + '>\n')
+                rec_func(item[k])
+                sys.stdout.write('</' + k + '>\n')
+            else:
+                sys.stdout.write('<' + k + '>' + item[k] + '</' + k + '>\n')
         sys.stdout.write('</li>\n')
     sys.stdout.write('</ul>\n')
+
+with open('agima/source.json','r') as f:
+    json_obj = json.loads(f.read())
+    rec_func(json_obj)
+    
