d9a860b45a81746d4416304ebc4003b911d0c647
ErshovYS <yurikzz@narod.ru>
2018-05-09 13:05:53 +0300

    Task 2. Agima

--- Agima_test/agima-test.py
+++ Agima_test/agima-test.py
@@ -3,10 +3,8 @@ import json
 import sys
 
 with open('agima/source.json','r') as f:
-    tag_dict = {'title': 'h1', 'body': 'p'}
-    json_text = f.read()
-    json_obj = json.loads(json_text)
+    json_obj = json.loads(f.read())
     for item in json_obj:
         for k in item.keys():
-            sys.stdout.write('<' + tag_dict[k] + '>' + item[k] + '</' + tag_dict[k] + '>\n')
+            sys.stdout.write('<' + k + '>' + item[k] + '</' + k + '>\n')
     
