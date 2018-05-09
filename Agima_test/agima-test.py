efa396c2151c189f4dd276b578d71f87d723a556
ErshovYS <yurikzz@narod.ru>
2018-05-09 13:25:24 +0300

    Task 3.

--- Agima_test/agima-test.py
+++ Agima_test/agima-test.py
@@ -4,7 +4,10 @@ import sys
 
 with open('agima/source.json','r') as f:
     json_obj = json.loads(f.read())
+    sys.stdout.write('<ul>\n')
     for item in json_obj:
+        sys.stdout.write('<li>\n')
         for k in item.keys():
             sys.stdout.write('<' + k + '>' + item[k] + '</' + k + '>\n')
-    
+        sys.stdout.write('</li>\n')
+    sys.stdout.write('</ul>\n')
